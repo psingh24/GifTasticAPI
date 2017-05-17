05b0771df51e880391f2b6212d6932193f0ad37b
Prabhdeep <ps591990@gmail.com>
2017-05-16 22:19:09 -0500

    logic finished

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var arrayOfButtons =["Michael", "Dwight", "Jim", "Pam"];
+var arrayOfButtons =["Dennis Reynolds", "Charlie Kelly", "Dee Reynolds", "Its Always Sunny"];
 
 
 
@@ -9,21 +9,67 @@ function renderingButtons(){
 
 
     for (var i=0; i< arrayOfButtons.length; i++){
-    $(".buttons").append("<button class='btn btn-danger office' data-name='"+arrayOfButtons[i]+"'>"+arrayOfButtons[i]+"</button>")
+    $(".buttons").append("<button class='btn btn-danger sunny' data-name='"+arrayOfButtons[i]+"'>"+arrayOfButtons[i]+"</button>")
 
 
 
 }
 }
 
+function ajaxCall(){
+
+        var character = $(this).attr("data-name");
+
+       
+        var url = "http://api.giphy.com/v1/gifs/search?";
+        var query = "q="+character;
+        var apiKeyandLimit = "&api_key=dc6zaTOxFJmzC&limit=10";
+      
+
+$.ajax({
+
+    url: url+query+apiKeyandLimit,
+    method: "GET"
+}).done(function(object){
+
+    var gif = object.data
+
+    for (var i=0; i< gif.length; i++) {
+            
+            var rating = gif[i].rating;
+			// console.log(rating)
+			var still = gif[i].images.fixed_height_still.url;
+			// console.log(still)
+			var animate = gif[i].images.fixed_height.url;
+			// console.log(animate)
+            $("#gifsArea").prepend("<div class='col-md-2 text-center giftastic'>  <img class='giff' data-still='"+still+"' data-animate='"+animate+"' data-state='still' src='"+ still+"'> <p class='rating'>Rating:  "+ rating +" </p> </div>")
+            
+    }
+    
+
+   
+})
+}
+
+$(document).on("click", ".giff", animateSwitch)
 
 
 
 
+function animateSwitch(){
+                var state = $(this).attr("data-state");
 
+                if( state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
 
+            }
 
-$(".submit").click(function(event){
+$(".add").click(function(event){
 
      event.preventDefault();
     var input = $("#search").val().trim();
@@ -35,9 +81,9 @@ $(".submit").click(function(event){
 
 
 
-$(document).on("click", ".office", function(){
-    console.log("test")
-})
+$(document).on("click", ".sunny", ajaxCall) 
+$(document).on("click", ".giff", animateSwitch)
+
   renderingButtons()
-console.log(arrayOfButtons)
+// console.log(arrayOfButtons)
 })
